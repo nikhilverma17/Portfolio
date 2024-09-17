a5aed1e3cb9c56b322e887459ec9d36f2bd0762b
Nikhil Verma <iamvermanikhil@gmail.com>
2024-09-17 10:45:35 +0530

    update
    update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,20 @@
 import React from "react";
-import me from '../assets/img/Resume.jpg'
-import '../components/resume.scss'
+import me from '../assets/img/Resume.jpg'; // Path to your image file
+import resumePdf from '../assets/pdf/Resume.pdf'; // Path to your PDF file
+import '../components/resume.scss';
 
-function Resume(){
-    return(
-        <img className="image" src={me}></img>
-    )
+function Resume() {
+    return (
+        <div className="resume-container">
+            {/* Download Button above the image */}
+            <a href={resumePdf} download="Resume.pdf" className="download-button">
+                Download Resume 
+            </a>
+
+            {/* Display Image */}
+            <img className="image" src={me} alt="Resume" />
+        </div>
+    );
 }
-export default Resume
+
+export default Resume;
